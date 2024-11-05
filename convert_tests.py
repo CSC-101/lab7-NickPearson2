8b69fba6aca58a04893277cef4556f38524558df
Nick Pearson <pearsonnick83@gmail.com>
2024-11-05 10:55:14 -0800

    Completed steps 1 and 2

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,15 @@
+import unittest
+import convert
 
+class TestCases(unittest.TestCase):
+    def test_str_to_float(self):
+        self.assertEqual(convert.str_to_float("1.2"), 1.2)
+        self.assertEqual(convert.str_to_float("Nice"), None)
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
