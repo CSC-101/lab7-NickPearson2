ed9337f86e31cc68b38751e8db333d0b52a657d8
Nick Pearson <pearsonnick83@gmail.com>
2024-11-07 10:43:48 -0800

    Completed lab7

--- command_line.py
+++ command_line.py
@@ -2,3 +2,10 @@ import sys
 
 if __name__ == '__main__':
     print(sys.argv)
+    total = 0
+    for arg in sys.argv:
+        try:
+            total += float(arg)
+        except ValueError:
+            print("Enter number")
+    print(total)
